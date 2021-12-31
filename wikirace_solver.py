1d2907b70c11c09f3053253456d0246afee8b65a
brend3n <brenden@knights.ucf.edu>
2021-12-31 00:06:04 -0500

    Most of the logic is implemented

--- wikirace_solver.py
+++ wikirace_solver.py
@@ -1,4 +1,6 @@
 import wiki_tool as wt
+import requests
+from bs4 import BeautifulSoup
 
 limit = 99999
 
@@ -13,14 +15,15 @@ def get_all_parent_links(title, limit):
     p_links = wt.get_all_links(soup)
     p_links = list(set(p_links))
 
-    for link in p_links:
-        print(link)
+    #for link in p_links:
+    #    print(link)
 
-    print(f"\n\n\nNumber of pages: {len(p_links)}")
+    # print(f"\n\n\nNumber of pages: {len(p_links)}")
 
     return p_links
 
-
+# This is a backtracking solution
+"""
 def find_path_exec(source, target):
     path = []
     cache = []
@@ -41,6 +44,7 @@ def find_path_exec(source, target):
     # Start search for source
     find_path(source, target, path, cache)
 
+
 def find_path(source, curr_target, path, cache):
 
     # Get all parents of curr_target
@@ -51,6 +55,7 @@ def find_path(source, curr_target, path, cache):
         path.insert(0,source)
         print(f"Path found: {path}")
         found_path = True
+        return
     else:
         # If we don't find what we are looking for it
         for parent in parents:
@@ -64,14 +69,87 @@ def find_path(source, curr_target, path, cache):
                 # Remove from path if not
                 path.pop(0)
                 cache.remove(parent)
+"""
+
+def get_soup(url):
+    # print(f"get_soup({url})")
+    headers = {'User-Agent':'Mozilla/5.0'}
+    page = requests.get(url, headers=headers)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    return soup
+
+def get_all_links(page_title):
+
+	page_title = str(page_title).replace(" ","_")
+	url = f"https://en.wikipedia.org/wiki/{page_title}"
+	soup = get_soup(url)
+	links = []
+	for link in soup.find_all('a'):
+		l = link.get('href')
+		l = str(l)
+		if ("/wiki/" in l) and (":" not in l) and (".svg" not in l) and (".org" not in l) and ("Main_Page" not in l):
+			l = l[6:]
+			links.append(l)
+	return links
 
 
+def find_shortest_path_helper(s,t):
+    wiki_search = []
+    reconstructed_path = []
+    source_links = get_all_links(s)
+    
+    if t in source_links:
+        print(f"Found path {s}->{t}")
+        return [s,t]
+    
+    wiki_search.append({
+        "parent":s,
+        "children":source_links
+    })
+    print(f"wiki_search: {wiki_search}")
+    
+    path = find_shortest_path(s,t,wiki_search,reconstructed_path)
+    print(f"Path: {path}")
+    return path
 
+# Source is the starting node and the target is the ending node.
+def find_shortest_path(source, target, wiki_search,re_path):
+    index = len(wiki_search) - 1
+
+    # Look for target
+    info = type(wiki_search[index])
+    print(f"typeof(wiki_search[index]): {info}")
+    for ele in wiki_search[index]:
+        print(f"ele: {ele}")
+        if target in ele["children"]:
+            print(f"Found target: {target}")
+            # re_path = reconstruct_path(source, target, wiki_search, re_path)
+            return re_path
+
+    # Target was not found, so get new data and update wiki_search
+    new_level = []
+    for parent in wiki_search[index]:
+        for child in parent["children"]:
+            data = {
+                "parent": child,
+                "children": get_all_links(child)
+            }
+            new_level.append(data)
+    wiki_search.append(new_level)
+    find_shortest_path(source, target, wiki_search, re_path)
+
+def reconstruct_path(source, target, wiki_search, re_path):
+    pass		
 
 def main():
-    source = input("Enter starting page: ")
-    target = input("Enter target page: ")
-    find_path_exec(source, target)
+    source = str(input("Enter starting page: "))
+    target = str(input("Enter target page: "))
+    
+    # This was the backtracking solution which we arent going to use
+    #find_path_exec(source, target)
+    print(f"Looking for path from {source} to {target}")
+    find_shortest_path_helper(source, target)
   
 
 
